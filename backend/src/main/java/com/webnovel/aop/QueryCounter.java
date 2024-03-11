7d124062bf11abb375d3cadb01b9263d1f99d77c
kim-jingyu <swc05161@gmail.com>
2024-03-11 13:03:37 +0900

    feat: logger 설정

---
+++ backend/src/main/java/com/webnovel/aop/QueryCounter.java
@@ -0,0 +1,15 @@
+package com.webnovel.aop;
+
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Getter
+@Component
+@RequestScope
+public class QueryCounter {
+    private int count;
+    public void increaseCount() {
+        count++;
+    }
+}
