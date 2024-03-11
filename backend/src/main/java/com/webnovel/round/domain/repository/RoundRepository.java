2ade8cf4e77d78bd8484301e9d0b0c119e9a4ce7
kim-jingyu <swc05161@gmail.com>
2024-03-11 15:32:24 +0900

    feat: Round 도메인 생성

---
+++ backend/src/main/java/com/webnovel/round/domain/repository/RoundRepository.java
@@ -0,0 +1,12 @@
+package com.webnovel.round.domain.repository;
+
+import com.webnovel.round.domain.Round;
+import com.webnovel.round.domain.exception.RoundNonFoundException;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoundRepository extends JpaRepository<Round, Long> {
+    default Round getById(Long roundId) {
+        return findById(roundId)
+                .orElseThrow(RoundNonFoundException::new);
+    }
+}
