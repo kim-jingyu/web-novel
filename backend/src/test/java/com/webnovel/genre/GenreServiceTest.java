58142118c17a577907cb97b636e0fcfaa5ad03ef
asdf <dlatldydap@gmail.com>
2024-03-15 17:52:42 +0900

    fix genre 테스트케이스

--- backend/src/test/java/com/webnovel/genre/GenreServiceTest.java
+++ backend/src/test/java/com/webnovel/genre/GenreServiceTest.java
@@ -3,12 +3,11 @@ package com.webnovel.genre;
 import com.webnovel.genre.domain.Genre;
 import com.webnovel.genre.domain.GenreType;
 import com.webnovel.genre.domain.dto.CreateGenreDto;
-import com.webnovel.genre.domain.dto.GenreDto;
+import com.webnovel.genre.domain.dto.FindGenreDto;
 import com.webnovel.genre.domain.service.GenreService;
 import com.webnovel.novel.domain.Novel;
 import com.webnovel.novel.domain.service.NovelService;
 import lombok.extern.slf4j.Slf4j;
-import org.ietf.jgss.GSSName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,7 +25,7 @@ public class GenreServiceTest {
 
     @Test
     void saveTest(){
-        Novel byNovelId1 = novelService.findByNovelId(1L);
+        Novel byNovelId1 = novelService.findByNovelId(2L);
         List<GenreType> genreType = new ArrayList<>();
         genreType.add(GenreType.ROMANCE);
         genreType.add(GenreType.ACTION);
@@ -34,23 +33,23 @@ public class GenreServiceTest {
 
         CreateGenreDto createGenreDto = CreateGenreDto.builder()
                 .genreTypes(genreType)
-                .novel(byNovelId1)
+                .novelId(byNovelId1.getNovelId())
                 .build();
         genreService.save(createGenreDto);
     }
     @Test
     void GenreTest(){
 
-        Novel byNovelId1 = novelService.findByNovelId(1L);
+        Novel byNovelId1 = novelService.findByNovelId(2L);
 
 
         GenreType genreType = GenreType.ACTION;
-        GenreDto genreDto = GenreDto.builder()
-                .novel(byNovelId1)
+        FindGenreDto findGenreDto = FindGenreDto.builder()
+                .novelId(byNovelId1.getNovelId())
                 .genreTypes(genreType)
                 .build();
 
-        Genre bynovel = genreService.findBynovel(genreDto);
+        Genre bynovel = genreService.findBynovel(findGenreDto);
         log.info(bynovel.getGenreTypes().toString());
     }
 
